6d2766ddf652f3dcb844c6ee83e22e3eec0dc158
Lucas Machado <lucasmachado.rj@gmail.com>
2024-06-24 16:43:12 +0000

    adjust build

--- ThePhasesOfCraftship/2_best_practice_first/deployment/assignment/end/packages/frontend/src/pages/mainPage.tsx
+++ ThePhasesOfCraftship/2_best_practice_first/deployment/assignment/end/packages/frontend/src/pages/mainPage.tsx
@@ -10,6 +10,7 @@ export const MainPage = () => {
   const loadPosts = async () => {
     try {
       const response = await api.posts.getPosts('recent');
+      // @ts-ignore
       setPosts(response.data.posts)
     } catch (err) {
       console.log(err);
